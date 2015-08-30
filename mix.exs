a830661d7b250a79c175d846193472f95e4cf554
akihiro17 <coolwizard11@gmail.com>
2015-08-30 23:31:28 +0900

    Make our sequence program into an OTP application

--- mix.exs
+++ mix.exs
@@ -14,8 +14,12 @@ defmodule Sequence.Mixfile do
   #
   # Type `mix help compile.app` for more information
   def application do
-    [applications: [:logger],
-     mod: {Sequence, []}]
+    [
+      applications: [:logger],
+      mod: {Sequence, []},
+      registered: [Sequence.Server],
+      env: [initial_number: 456]
+     ]
   end
 
   # Dependencies can be Hex packages:
